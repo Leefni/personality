266d81a01ef200550e800bcad2fe4807a29418ac
Leefni <138658419+Leefni@users.noreply.github.com>
2026-03-18 15:26:50 +0100

    Load cache helpers in v1 common bootstrap

--- api/v1/common.php
+++ api/v1/common.php
@@ -8,6 +8,7 @@ require __DIR__ . '/../../src/QuizService.php';
 require __DIR__ . '/http/response.php';
 require __DIR__ . '/http/request.php';
 require __DIR__ . '/http/visitor.php';
+require __DIR__ . '/cache.php';
 require __DIR__ . '/results_cache.php';
 
 $quizRepository = new QuizRepository($pdo);
